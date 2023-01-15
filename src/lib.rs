b95e43e8dd8338ec9d7b7263b00e215ea9c2096c
fnk <faun@fauns.space>
2023-01-15 21:31:05 +0100

    Fix naming  collision of exposed function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
  #[no_mangle]
-extern "C" fn quad_url_crate_version() -> u32 {
+extern "C" fn quad_timestamp_crate_version() -> u32 {
     let major = env!("CARGO_PKG_VERSION_MAJOR").parse::<u32>().unwrap();
     let minor = env!("CARGO_PKG_VERSION_MINOR").parse::<u32>().unwrap();
     let patch = env!("CARGO_PKG_VERSION_PATCH").parse::<u32>().unwrap();
